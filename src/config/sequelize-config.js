369fac40a0f722c26b6a15335d067273872c4ec5
ELENAMARIAVARGHESE <elenamariavarghese@gmail.com>
2024-02-08 09:39:58 +0530

    created api

--- src/config/sequelize-config.js
+++ src/config/sequelize-config.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize({
   database: 'e_commerce',
   username: 'root',
-  password: '1994',
+  password: 'experion@123',
   host: '127.0.0.1',
   dialect: 'mysql',
 });
--- src/models/ec_suppliers.js
+++ src/models/ec_suppliers.js
@@ -16,6 +16,7 @@ const ec_suppliers = sequelize.define('ec_suppliers', {
   e_mail:{
     type:DataTypes.STRING,
     allowNull: false,
+    unique:true,
   },
   password: {
     type: DataTypes.STRING,
@@ -28,12 +29,26 @@ const ec_suppliers = sequelize.define('ec_suppliers', {
   },
   registration_id:{
     type:DataTypes.STRING,
-    allowNull: false,
+    allowNull: true,
+    defaultValue:()=>{
+        return Math.floor(100000 + Math.random()*900000).toString();
+    }
   },
   registration_time_stamp:{
     type:DataTypes.DATE,
     allowNull: false,
-  }
+    defaultValue:sequelize.literal('CURRENT_TIMESTAMP'),
+  },
+  createdAt:{
+    type:DataTypes.DATE,
+    allowNull: false,
+    defaultValue:sequelize.literal('CURRENT_TIMESTAMP'),
+  },
+  updatedAt:{
+    type:DataTypes.DATE,
+    allowNull: false,
+    defaultValue:sequelize.literal('CURRENT_TIMESTAMP'),
+  },
  
 });
 
